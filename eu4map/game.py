f73f1dbf146966aef71f1b5dfcdc026a2c36a5dd
icicel <isakham@chalmers.se>
2024-09-20 14:33:24 +0200

    Mod path collection

--- eu4map/game.py
+++ eu4map/game.py
@@ -1,4 +1,6 @@
+import json
 import os
+import eu4map.parse as parse
 import eu4map.provincemap as provincemap
 
 # Represents an EU4 file system
@@ -7,12 +9,27 @@ class Game:
     path: str
     modPaths: list[str]
 
-    # takes the game directory and optionally a list of mod directories
-    def __init__(self, path: str, modPaths: list[str] = []):
-        if modPaths:
-            raise NotImplementedError("Use vanilla plz :>")
-        self.path = path
-        self.modPaths = modPaths
+    # takes the steam directory (where game files are stored) and documents directory (where mods are stored)
+    def __init__(self, steamPath: str, documentsPath: str):
+        self.path = steamPath
+
+        # get descriptors
+        dlcLoadPath = os.path.join(documentsPath, "dlc_load.json")
+        with open(dlcLoadPath, 'r') as file:
+            dlcLoad: dict = json.load(file)
+        descriptorPaths = [os.path.join(documentsPath, descriptorPath) for descriptorPath in dlcLoad["enabled_mods"]]
+
+        # get mod paths
+        self.modPaths = []
+        for descriptorPath in descriptorPaths:
+            descriptor = parse.parse(descriptorPath, allowDuplicates=True)
+            if "path" in descriptor:
+                modPath = descriptor["path"]
+            elif "archive" in descriptor:
+                modPath = descriptor["archive"]
+            else:
+                raise KeyError(f"No path in descriptor: {descriptorPath}")
+            self.modPaths.append(modPath)
     
     def getProvinceMap(self) -> provincemap.ProvinceMap:
         return provincemap.ProvinceMap(os.path.join(self.path, "map/provinces.bmp"))
--- test.py
+++ test.py
@@ -1,8 +1,11 @@
+import os
 import eu4map.game as game
 
 GAME_DIRECTORY = "C:/Program Files (x86)/Steam/steamapps/common/Europa Universalis IV"
+DOCUMENTS_DIRECTORY = os.path.expanduser("~/Documents/Paradox Interactive/Europa Universalis IV")
 
-eu4 = game.Game(GAME_DIRECTORY)
+eu4 = game.Game(GAME_DIRECTORY, DOCUMENTS_DIRECTORY)
+print(eu4.modPaths)
 
 pmap = eu4.getProvinceMap()
 pmap.borderize().save("output.bmp")
