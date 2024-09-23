9045108381497116fab179fa0c4efbad7f0695a7
icicel <isakham@chalmers.se>
2024-09-24 01:15:39 +0200

    Made borderization slightly nicer

--- eu4map/bitmap.py
+++ eu4map/bitmap.py
@@ -1,5 +1,6 @@
 import PIL.Image as img
 import PIL.ImageChops as chops
+import PIL.ImageDraw as draw
 import eu4map.files as files
 
 # a bitmap where each RGB color represents a province
@@ -7,37 +8,41 @@ class ProvinceMap:
     image: img.Image
     def __init__(self, files: files.Files):
         provinces_bmp = files.provinces_bmp()
-        # add an alpha channel for compositing
-        self.image = img.open(provinces_bmp).convert("RGBA")
+        self.image = img.open(provinces_bmp)
 
     # create a b&w border image from the province map
     # its mode is "L" (grayscale)
     def borderize(self) -> img.Image:
         # create shift-difference images for each direction
-        shiftDown = self.shiftDifference(0, -1)
-        shiftRight = self.shiftDifference(-1, 0)
-        shiftDownRight = self.shiftDifference(-1, -1)
+        shiftDown = self.shiftDifference(0, 1)
+        shiftRight = self.shiftDifference(1, 0)
+        shiftDownRight = self.shiftDifference(1, 1)
         # merge all 9 image bands into one grayscale image
+        # the only non-black pixels in the result are the borders
         bands = shiftDown.split() + shiftRight.split() + shiftDownRight.split()
         borders = bands[0]
         for band in bands[1:]:
             borders = chops.add(borders, band)
-        # set black to white and non-black to black
-        return borders.point(lambda p: 0 if p else 255)
+        # grayscale, then set black to white and non-black to black
+        return borders.convert("L").point(lambda p: 0 if p else 255)
     
     # create and overlay a border image on the province map
     def borderOverlay(self) -> img.Image:
         borders = self.borderize()
-        return img.composite(self.image, borders.convert("RGBA"), borders)
+        return img.composite(self.image, borders.convert("RGB"), borders)
 
-    # returns pixel difference between the map and a shifted version of itself
+    # returns pixel difference between the map and itself shifted down-rightwards
+    # if a pixel is non-black in the difference image, 
+    # it means its color changed between the original and the shifted image
     def shiftDifference(self, shiftX: int, shiftY: int) -> img.Image:
         shifted = self.image.copy().transform(
             self.image.size, 
             img.Transform.AFFINE, 
-            (1, 0, shiftX, 0, 1, shiftY),
-            fillcolor=(0, 0, 0, 0)) # transparent black
-        # fill outside the transform with the original image
-        shifted = img.alpha_composite(self.image, shifted)
-        # difference with original image
-        return chops.difference(self.image, shifted)
+            (1, 0, -shiftX, 0, 1, -shiftY))
+        diff = chops.difference(self.image, shifted)
+        # set pixels outside the shifted image's range to black
+        if shiftX:
+            draw.Draw(diff).rectangle((0, 0, shiftX - 1, diff.height), fill=0)
+        if shiftY:
+            draw.Draw(diff).rectangle((0, 0, diff.width, shiftY - 1), fill=0)
+        return diff
