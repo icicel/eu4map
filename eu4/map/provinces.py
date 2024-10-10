6fca060e329fb0f45b437dfade8e62e7dc2125c7
icicel <isakham@chalmers.se>
2024-10-10 15:51:50 +0200

    Move image processing to separate module

--- eu4/map/provinces.py
+++ eu4/map/provinces.py
@@ -1,6 +1,4 @@
 import PIL.Image as img
-import PIL.ImageChops as chops
-import PIL.ImageDraw as draw
 import eu4.game as game
 
 # a bitmap where each RGB color represents a province
@@ -9,40 +7,3 @@ class ProvinceMap:
     def __init__(self, game: game.Game):
         provinces_bmp = game.getFile("map/provinces.bmp")
         self.image = img.open(provinces_bmp)
-
-    # create a b&w border image from the province map
-    # its mode is "L" (grayscale)
-    def borderize(self) -> img.Image:
-        # create shift-difference images for each direction
-        shiftDown = self.shiftDifference(0, 1)
-        shiftRight = self.shiftDifference(1, 0)
-        shiftDownRight = self.shiftDifference(1, 1)
-        # merge all 9 image bands into one grayscale image
-        # the only non-black pixels in the result are the borders
-        bands = shiftDown.split() + shiftRight.split() + shiftDownRight.split()
-        borders = bands[0]
-        for band in bands[1:]:
-            borders = chops.add(borders, band)
-        # grayscale, then set black to white and non-black to black
-        return borders.convert("L").point(lambda p: 0 if p else 255)
-    
-    # create and overlay a border image on the province map
-    def borderOverlay(self) -> img.Image:
-        borders = self.borderize()
-        return img.composite(self.image, borders.convert("RGB"), borders)
-
-    # returns pixel difference between the map and itself shifted down-rightwards
-    # if a pixel is non-black in the difference image, 
-    # it means its color changed between the original and the shifted image
-    def shiftDifference(self, shiftX: int, shiftY: int) -> img.Image:
-        shifted = self.image.copy().transform(
-            self.image.size, 
-            img.Transform.AFFINE, 
-            (1, 0, -shiftX, 0, 1, -shiftY))
-        diff = chops.difference(self.image, shifted)
-        # set pixels outside the shifted image's range to black
-        if shiftX:
-            draw.Draw(diff).rectangle((0, 0, shiftX - 1, diff.height), fill=0)
-        if shiftY:
-            draw.Draw(diff).rectangle((0, 0, diff.width, shiftY - 1), fill=0)
-        return diff
--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from eu4 import game
+from eu4 import image
 from eu4.map import provinces
 
 eu4 = game.Game(modloader=True)
 print(eu4.loadOrder)
 
 pmap = provinces.ProvinceMap(eu4)
-pmap.borderize().save("output.bmp")
+image.borderize(pmap.image).save("output.bmp")
 
 eu42 = game.Game(mod=3327591954)
 print(eu42.loadOrder)
