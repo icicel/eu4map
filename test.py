902d9336242b9601930802f720a57b6bc9a4a293
icicel <isakham@chalmers.se>
2024-10-14 17:55:18 +0200

    Rename ProvinceMap module again again

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from eu4 import game
 from eu4 import image
-from eu4 import maps
+from eu4.maps import provinces
 
 eu4 = game.Game(modloader=True)
 print(eu4.loadOrder)
 
-pmap = maps.ProvinceMap(eu4)
+pmap = provinces.ProvinceMap(eu4)
 image.borderize(pmap.image).save("output.bmp")
 
 eu42 = game.Game(mod=3327591954)
