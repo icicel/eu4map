44e4ea54ed27c36005781bbe6058a1e8592d6718
icicel <isakham@chalmers.se>
2024-10-30 00:46:51 +0100

    Weird CSV parsing quirk fix

--- eu4/mapfiles.py
+++ eu4/mapfiles.py
@@ -29,10 +29,23 @@ class ProvinceDefinition(files.CsvFile):
         super().__init__(definitionPath)
         self.color = {}
         for province, red, green, blue, *_ in self:
-            self.color[int(province)] = (int(red), int(green), int(blue))
+            try:
+                self.color[int(province)] = (int(red), int(green), int(blue))
+            except ValueError: # see strToIntWeird below
+                self.color[int(province)] = (strToIntWeird(red), strToIntWeird(green), strToIntWeird(blue))
     
     def __getitem__(self, key: int) -> tuple[int, int, int]:
         return self.color[key]
+
+# For some reason, the EU4 CSV parser can successfully detect
+#  and remove non-digits from the end of a number
+# I have only seen this feature in action in the definition.csv
+#  for Voltaire's Nightmare (where "104o" is successfully parsed as 104)
+# I have no idea why it exists or was ever deemed necessary to implement
+def strToIntWeird(value: str) -> int:
+    while not value[-1].isdigit():
+        value = value[:-1]
+    return int(value)
 
 
 # Contains arrays for:
