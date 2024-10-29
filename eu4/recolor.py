ad086c06ea8a99b8184f70ab3bb4174eb0dc73e3
icicel <isakham@chalmers.se>
2024-10-29 18:13:06 +0100

    Guarantee unique shades of white

--- eu4/recolor.py
+++ eu4/recolor.py
@@ -8,29 +8,36 @@ from typing import Generator
 
 # Special colors to use in recoloring
 # DEFAULT - the default color of the province map
-# SHADES_OF_WHITE - a shade of white (unique among other SHADES_OF_WHITE colors)
+# SHADES_OF_WHITE - a unique shade of white
 class SpecialColor(enum.Enum):
     DEFAULT = 0
     SHADES_OF_WHITE = 1
 
 
 # Recolor a province map according to a mapping of province ID to color
+# Does not modify the original province map
 class Recolor:
     bitmap: img.Image
     definition: mapfiles.ProvinceDefinition
     colorMap: dict[tuple[int, int, int], tuple[int, int, int] | SpecialColor]
+    usedColors: set[tuple[int, int, int]]
     def __init__(self, provinces: mapfiles.ProvinceMap, definition: mapfiles.ProvinceDefinition):
         self.bitmap = provinces.bitmap.copy()
         self.definition = definition
         self.colorMap = {}
+        self.usedColors = set()
     
     def __setitem__(self, province, color: tuple[int, int, int] | SpecialColor):
         provinceColor = self.definition[province]
         self.colorMap[provinceColor] = color
+        if type(color) is tuple:
+            self.usedColors.add(color)
+        elif color is SpecialColor.DEFAULT:
+            self.usedColors.add(provinceColor)
     
     # Provinces not in the mapping are set to default
     def generate(self, default: tuple[int, int, int] | SpecialColor = SpecialColor.DEFAULT) -> image.RGB:
-        shadesOfWhiteGenerator = shadesOfWhite()
+        shadesOfWhiteGenerator = shadesOfWhite(self.usedColors)
         for y in range(self.bitmap.height):
             for x in range(self.bitmap.width):
                 pixelColor: tuple[int, int, int] = self.bitmap.getpixel((x, y)) # type: ignore
@@ -58,11 +65,12 @@ def turnTransparent(provinces: mapfiles.ProvinceMap, color: tuple[int, int, int]
 
 
 # Generates increasingly darker shades of white
-def shadesOfWhite() -> Generator[tuple[int, int, int], None, None]:
+# Excludes the used colors in the set from being generated
+def shadesOfWhite(usedColors: set[tuple[int, int, int]]) -> Generator[tuple[int, int, int], None, None]:
     # yikes
     for maxValue in range(0, 255 * 3):
         for r in range(0, maxValue + 1):
             for g in range(0, maxValue + 1):
                 for b in range(0, maxValue + 1):
-                    if r == maxValue or g == maxValue or b == maxValue:
+                    if (r == maxValue or g == maxValue or b == maxValue) and (r, g, b) not in usedColors:
                         yield (255 - r, 255 - g, 255 - b)
