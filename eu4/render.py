5eb4623f7f972906a81fc80c129ac7147bacc82e
icicel <isakham@chalmers.se>
2024-11-07 22:44:35 +0100

    Improve mask rendering efficiency

--- eu4/render.py
+++ eu4/render.py
@@ -11,36 +11,56 @@ def renderMasks(provinces: mapfiles.ProvinceMap) -> image.RGB:
     # this will not affect the ProvinceMap object
     provinces.masks.sort(key=lambda mask: (mask.mask.bitmap.height, mask.mask.bitmap.width), reverse=True)
 
-    # start with this width and height
-    SIZE = 6000
+    # start with the smallest possible square
+    SIZE = int(sum(mask.mask.bitmap.width * mask.mask.bitmap.height for mask in provinces.masks) ** 0.5)
 
-    maskmap = fitMasks(SIZE, provinces)
-    if not maskmap:
-        raise Exception("Failed to fit masks into a square")    
-    return image.RGB(maskmap)
+    while True:
+        maskmap = _fitMasks(SIZE, provinces)
+        if maskmap:
+            return image.RGB(maskmap)
+        SIZE += 50
 
+def _tooWide(x: int, width: int, size: int) -> bool:
+    return x + width + 2 > size
+def _tooTall(y: int, height: int, size: int) -> bool:
+    return y + height + 2 > size
 
 # Try to fit the map's province masks into a square of the given size
 # Returns None if the masks cannot fit
-def fitMasks(squareSize: int, provinces: mapfiles.ProvinceMap) -> img.Image | None:
+def _fitMasks(squareSize: int, provinces: mapfiles.ProvinceMap) -> img.Image | None:
     square = img.new("RGB", (squareSize, squareSize), (255, 0, 0))
     x: int = squareSize # start as squareSize to force a new row on the first mask
     y: int = 0
-    nextHeight = 0
+    nextRowX: int = 0
+    nextRowY: int = 0
     for mask in provinces.masks:
         maskWidth, maskHeight = mask.mask.bitmap.size
-        # create new row if too wide
-        if x + maskWidth + 2 >= squareSize:
-            x = 0
-            y = nextHeight
-            nextHeight += maskHeight + 2
-            # abort if still too wide
-            if x + maskWidth + 2 >= squareSize:
-                return None
-        # move the mask up as much as possible
-        while y >= 0 and square.getpixel((x, y)) == (255, 0, 0) and square.getpixel((x + maskWidth + 1, y)) == (255, 0, 0):
-            y -= 1
-        y += 1
+        if maskWidth > squareSize or maskHeight > squareSize:
+            return None
+        if _tooWide(x, maskWidth, squareSize):
+            # move to the next row
+            x = nextRowX
+            y = nextRowY
+            nextRowY += maskHeight + 2
+            if _tooTall(y, maskHeight, squareSize):
+                # nudge row to the right
+                while _tooTall(y, maskHeight, squareSize):
+                    x += 1
+                    if _tooWide(x, maskWidth, squareSize):
+                        return None
+                    while True:
+                        y -= 1
+                        if y < 0 or square.getpixel((x, y)) != (255, 0, 0) or square.getpixel((x + maskWidth + 1, y)) != (255, 0, 0):
+                            y += 1
+                            break
+                nextRowX = x
+                nextRowY = y + maskHeight + 2
+        else:
+            while True:
+                y -= 1
+                if y < 0 or square.getpixel((x, y)) != (255, 0, 0) or square.getpixel((x + maskWidth + 1, y)) != (255, 0, 0):
+                    y += 1
+                    break
         # paste the mask with a 1 pixel green border
         square.paste(mask.mask.bitmap, (x + 1, y + 1))
         draw.Draw(square).rectangle([x, y, x + maskWidth + 1, y + maskHeight + 1], outline=(0, 255, 0))
