dbef7d9cd3da54b8d702c99931d3f0b36273423b
icicel <isakham@chalmers.se>
2024-11-08 01:38:25 +0100

    Reduce mask render padding

--- eu4/render.py
+++ eu4/render.py
@@ -28,6 +28,7 @@ class Ledge:
         self.bottomSpace: int = bottomSpace
 
 # Try to fit the map's province masks into a square of the given size
+# Masks have padding of 1 pixel on all sides
 # Returns None if the masks cannot fit
 def _fitMasks(squareSize: int, provinces: mapfiles.ProvinceMap) -> img.Image | None:
     square = img.new("RGB", (squareSize, squareSize), (255, 0, 0))
@@ -39,14 +40,16 @@ def _fitMasks(squareSize: int, provinces: mapfiles.ProvinceMap) -> img.Image | N
 
     for mask in provinces.masks:
         maskWidth, maskHeight = mask.mask.bitmap.size
-        if maskWidth > squareSize or maskHeight > squareSize:
+        if maskWidth + 2 > squareSize or maskHeight + 2 > squareSize:
             return None
         
         # overflow to the right, start a new row
-        if x + maskWidth + 2 > squareSize:
+        if x + maskWidth + 1 > squareSize - 1:
             # go backwards until a fitting ledge is found
             for i, ledge in enumerate(reversed(ledges)):
-                if ledge.fit < maskHeight + 2: # starting the row here would create a lip
+                if ledge.x + maskWidth + 1 > squareSize - 1: # still overflowing
+                    continue
+                if maskHeight + 1 > ledge.fit: # starting the row here would create a lip
                     continue
                 x, y = ledge.x, ledge.y
                 # invalidate passed ledges, including this one
@@ -56,7 +59,7 @@ def _fitMasks(squareSize: int, provinces: mapfiles.ProvinceMap) -> img.Image | N
             else:
                 # go forwards until a ledge with enough bottom space is found
                 for i, ledge in enumerate(ledges):
-                    if ledge.bottomSpace < maskHeight + 2:
+                    if maskHeight + 1 > ledge.bottomSpace:
                         continue
                     x, y = ledge.x, ledge.y
                     # invalidate all ledges
@@ -70,18 +73,19 @@ def _fitMasks(squareSize: int, provinces: mapfiles.ProvinceMap) -> img.Image | N
         else:
             while True:
                 y -= 1
-                if y < 0 or square.getpixel((x, y)) != (255, 0, 0) or square.getpixel((x + maskWidth + 1, y)) != (255, 0, 0):
+                # check the top left/right corners of where the actual mask would be, not the padding
+                if y < 0 or square.getpixel((x+1, y+1)) != (255, 0, 0) or square.getpixel((x + maskWidth, y+1)) != (255, 0, 0):
                     y += 1
                     break
 
-        # paste the mask with a 1 pixel green border
+        # paste the mask with a 1 pixel green padding
         square.paste(mask.mask.bitmap, (x + 1, y + 1))
         draw.Draw(square).rectangle([x, y, x + maskWidth + 1, y + maskHeight + 1], outline=(0, 255, 0))
 
         # update ledges with the new one formed by this mask and the mask/image border to its left
         ledgeX = x
-        ledgeY = y + maskHeight + 2
-        bottomDistance = squareSize - ledgeY
+        ledgeY = y + maskHeight + 1
+        bottomDistance = squareSize - 1 - ledgeY
         if ledges:
             heightDifference = ledges[-1].y - ledgeY
             ledges.append(Ledge(ledgeX, ledgeY, heightDifference, bottomDistance))
@@ -89,6 +93,6 @@ def _fitMasks(squareSize: int, provinces: mapfiles.ProvinceMap) -> img.Image | N
             ledges.append(Ledge(ledgeX, ledgeY, bottomDistance, bottomDistance))
 
         # move to the right
-        x += maskWidth + 2
+        x += maskWidth + 1
 
     return square
