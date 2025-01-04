3838fe2fdebaac20ec53602dbaefa115d6e3d09a
icicel <isakham@chalmers.se>
2025-01-04 20:36:50 +0100

    Debug for creating terrain testing images

--- eu4/debug.py
+++ eu4/debug.py
@@ -1,9 +1,13 @@
+import random
 import re
 import os
+import PIL.Image as img
 
 from eu4 import game
+from eu4 import image
 from eu4 import mapfiles
 from eu4 import presets
+from eu4 import recolor
 from eu4 import render
 
 
@@ -59,3 +63,49 @@ def testAllMods():
         print(f"Loading mod {mod.name}... ({mod.technicalName})")
         eu4 = game.Game(mod=mod.path)
         testPresets(eu4, f"test/{dirname}")
+
+
+def generateTerrainTest(modloader: bool = False, mod: str | int | list[str | int] | None = None):
+
+    print("Loading game...")
+    eu4 = game.Game(modloader, mod)
+
+    print("Loading data...")
+    defaultMap = mapfiles.DefaultMap(eu4)
+    definition = mapfiles.ProvinceDefinition(eu4, defaultMap)
+    climate = mapfiles.Climate(eu4, defaultMap)
+    terrain = mapfiles.TerrainMap(eu4, defaultMap)
+    tree = mapfiles.TreeMap(eu4, defaultMap)
+
+    print("Loading map...")
+    provinceMap = mapfiles.ProvinceMap(eu4, defaultMap, definition)
+    
+    SIZE = 30
+    # grab X arbitrary land provinces
+    nonland = defaultMap.seas + defaultMap.lakes + climate.wastelands
+    land = [province for province in provinceMap.provinces if province not in nonland]
+    chosenProvinces = random.sample(land, SIZE*SIZE)
+    # grab an arbitrary wasteland province's color
+    wastelandColor = definition.color[climate.wastelands[0]]
+
+    print("Recoloring provinces...")
+    # color the chosen provinces in the wasteland's color
+    recolorBackground = recolor.Recolor(provinceMap, definition)
+    for province in chosenProvinces:
+        recolorBackground[province] = wastelandColor
+    provinceImage = recolorBackground.generate().bitmap
+
+    print("Overlaying trees...")
+    # overlay the tree map on the terrain map (ignoring black (palette index 0) pixels)
+    terrainImage = terrain.bitmap.convert("RGB")
+    terrainImage.paste(tree.resizedBitmap.convert("RGB"), mask=tree.resizedBitmap.point(lambda p: 1 if p else 0, mode="1"))
+
+    print("Generating pixels...")
+    # generate a pixel of color for each chosen province
+    # (this can then be placed in the province image where the terrain should be tested)
+    pixelImage = img.new("RGB", (SIZE, SIZE))
+    pixelImage.putdata([definition.color[province] for province in chosenProvinces])
+
+    provinceImage.show()
+    terrainImage.show()
+    pixelImage.show()
